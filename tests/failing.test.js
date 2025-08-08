7bb2ab92a32b86bc380ae0770dc1d65f7e02a920
talhabektas <mehmettalha.bektas@gmail.com>
2025-08-08 14:06:38 +0300

    chore(ci): workflow’u tetiklemek için küçük değişiklik

--- tests/failing.test.js
+++ tests/failing.test.js
@@ -1,7 +1,7 @@
 const { sum } = require('../src/index');
 
 test('intentional fail to trigger CI auto-fix', () => {
-  expect(sum(2, 2)).toBe(5); // bilerek başarısız
+    expect(sum(2, 2)).toBe(5); // bilerek başarısız
 });
 
 
